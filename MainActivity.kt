0f6783630a7f3426bd049bef25ebe1dd4d8dce36
miguelaalmeida1981-tech <miguelaalmeida1981@gmail.com>
2026-04-20 17:27:35 -0300

    Implement MainActivity with navigation setup

---
+++ MainActivity.kt
@@ -0,0 +1,24 @@
+package com.vestbem.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.vestbem.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = findNavController(R.id.bottom_navigation)
+        
+        bottomNav.setupWithNavController(navController)
+    }
+}
